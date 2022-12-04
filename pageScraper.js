55a968b27eae3a77209a4a9aa391f38b8d685146
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-04 13:13:19 +0200

    added pageScraper

---
+++ pageScraper.js
@@ -0,0 +1,10 @@
+const scrapperObject = {
+    url: https://virok.com.ua/ua,
+    async scrapper(browser) {
+        let page = await browser.newPage()
+        console.log(`Navigating to ${this.url}...`)
+        await page.goto(this.url)
+    }
+}
+
+module.exports = scrapperObject
