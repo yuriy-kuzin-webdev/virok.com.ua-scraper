61099bfcf14ae1e5b1bc5dc4ccecc067847ec231
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-04 16:42:57 +0200

    update to get detailed data from each link

--- pageScraper.js
+++ pageScraper.js
@@ -10,7 +10,32 @@ const scraperObject = {
             links = links.map(el => el.querySelector('.product-image > .img-caption .caption-block a').href)
             return links
         })
-        console.log(urls)
+
+        //Looping throught each of the links, open a new page instance and get the relevant data from them
+        const pagePromise = (link) => new Promise(async(resolve, reject) => {
+			let dataObj = {}
+			let newPage = await browser.newPage()
+			await newPage.goto(link)
+			dataObj['article'] = await newPage.$eval('#product_addtocart_form > div.product-shop > div.number-sku', text => {
+				// Strip new line and tab spaces
+				text = text.textContent.replace(/(\r\n\t|\n|\r|\t)/gm, "")
+				return text.split(':')[1]
+			});
+            
+			dataObj['imageUrl'] = await newPage.$eval('#image-main', img => img.src)
+			dataObj['description'] = await newPage.$eval('#product_addtocart_form > div.product-shop > div.product-name > span', span => span.textContent)
+			dataObj['category'] = await newPage.$eval('body > div > div > div.main-container.col1-layout > div > div.breadcrumbs > ul > li:nth-child(3) > a', a => a.title)
+			dataObj['type'] = await newPage.$eval('body > div > div > div.main-container.col1-layout > div > div.breadcrumbs > ul > li:nth-last-child(2) > a', a => a.title)
+
+			resolve(dataObj)
+			await newPage.close()
+		});
+
+        for(link in urls){
+			let currentPageData = await pagePromise(urls[link]);
+			// scrapedData.push(currentPageData);
+			console.log(currentPageData);
+		}
     }
 }
 
