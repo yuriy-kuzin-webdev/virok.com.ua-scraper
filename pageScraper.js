cc291f256cdfd9de6288caca1a973e777d03be76
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-04 14:43:22 +0200

    get all the links

--- pageScraper.js
+++ pageScraper.js
@@ -1,9 +1,16 @@
 const scraperObject = {
-    url: 'https://virok.com.ua/ua',
+    url: 'https://virok.com.ua/ua/katalog.html',
     async scraper(browser) {
         let page = await browser.newPage()
         console.log(`Navigating to ${this.url}...`)
         await page.goto(this.url)
+        // Wait for the required DOM to be rendered
+        await page.waitForSelector('.jet')
+        let urls = await page.$$eval('li.item.last', links => {
+            links = links.map(el => el.querySelector('.product-image > .img-caption .caption-block a').href)
+            return links
+        })
+        console.log(urls)
     }
 }
 
